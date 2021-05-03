84b8ca6167a8a221ffece6b667a8cf685d541dd6
Murari Gupta <murarigupta2367@gmail.com>
2021-05-03 15:01:10 +0545

    prettier error fixed

--- src/utilities/diffCalculator.js
+++ src/utilities/diffCalculator.js
@@ -1,29 +1,29 @@
 import { diffWords } from 'diff';
 
 function getDiffByCharacter(diff) {
-  const different = diff.reduce(
-    (accumulator, words) => {
-      const wordNum = words.value.length;
-      if (!(words.added || words.removed)) {
-        if (words.value.trim()) {
-          return {
-            ...accumulator,
-            charMatched: accumulator.charMatched + wordNum,
-          };
-        }
-        return accumulator;
-      }
-      return accumulator;
-    },
-    { charMatched: 0 },
-  );
-  return different;
+    const different = diff.reduce(
+        (accumulator, words) => {
+            const wordNum = words.value.length;
+            if (!(words.added || words.removed)) {
+                if (words.value.trim()) {
+                    return {
+                        ...accumulator,
+                        charMatched: accumulator.charMatched + wordNum,
+                    };
+                }
+                return accumulator;
+            }
+            return accumulator;
+        },
+        { charMatched: 0 },
+    );
+    return different;
 }
 
 export default function (transcriberText, originalText) {
-  const filteredOriginalText = originalText.replace(/\[(.*?)\]/g, '');
-  const diff = diffWords(transcriberText, filteredOriginalText);
-  const { charMatched } = getDiffByCharacter(diff);
-  const accuracy = (charMatched / filteredOriginalText.length) * 100;
-  return [+(100 - accuracy).toFixed(2), +accuracy.toFixed(2)];
+    const filteredOriginalText = originalText.replace(/\[(.*?)\]/g, '');
+    const diff = diffWords(transcriberText, filteredOriginalText);
+    const { charMatched } = getDiffByCharacter(diff);
+    const accuracy = (charMatched / filteredOriginalText.length) * 100;
+    return [+(100 - accuracy).toFixed(2), +accuracy.toFixed(2)];
 }
