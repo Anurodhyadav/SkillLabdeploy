047e9fc83fe88c7d30128fc0ac74bc4d4c8dd288
Murari Gupta <murarigupta2367@gmail.com>
2021-05-04 11:59:10 +0545

    removed unwanted code

--- src/views/TempAdmin.vue
+++ src/views/TempAdmin.vue
@@ -36,7 +36,10 @@
                         <div>
                             <span>
                                 <ul class="list">
-                                    <li>Hidden Students: {{ hiddenstudentCount }}</li>
+                                    <li v-if="hiddenstudentCount === 0">
+                                        Hidden Students: No students hidden yet
+                                    </li>
+                                    <li v-else>Hidden Students: {{ hiddenstudentCount }}</li>
                                 </ul>
                             </span>
                         </div>
@@ -175,8 +178,6 @@ export default {
     name: 'Admin',
     components: {
         NavBar,
-        // mdbHorizontalBarChart,
-        // mdbContainer,
         AudioDump,
     },
     data() {
@@ -200,55 +201,6 @@ export default {
             hiddenstudentCount: 0,
             visiblestudentCount: 0,
             adminCount: 0,
-            // horizontalBarChartData: {
-            //   labels: ['Stat a', 'Stat b', 'Stat c'],
-            //   datasets: [
-            //     {
-            //       label: '',
-            //       data: [12, 19, 3, 5, 2, 3],
-            //       backgroundColor: [
-            //         'rgba(255, 99, 132, 0.2)',
-            //         'rgba(54, 162, 235, 0.2)',
-            //         'rgba(255, 206, 86, 0.2)',
-            //         'rgba(75, 192, 192, 0.2)',
-            //         'rgba(153, 102, 255, 0.2)',
-            //         'rgba(255, 159, 64, 0.2)',
-            //       ],
-            //       borderColor: [
-            //         'rgba(255,99,132,1)',
-            //         'rgba(54, 162, 235, 1)',
-            //         'rgba(255, 206, 86, 1)',
-            //         'rgba(75, 192, 192, 1)',
-            //         'rgba(153, 102, 255, 1)',
-            //         'rgba(255, 159, 64, 1)',
-            //       ],
-            //       borderWidth: 1,
-            //     },
-            //   ],
-            // },
-            // horizontalBarChartOptions: {
-            //   responsive: false,
-            //   maintainAspectRatio: false,
-            //   scales: {
-            //     xAxes: [
-            //       {
-            //         barPercentage: 1,
-            //         gridLines: {
-            //           display: true,
-            //           color: 'rgba(0, 0, 0, 0.1)',
-            //         },
-            //       },
-            //     ],
-            //     yAxes: [
-            //       {
-            //         gridLines: {
-            //           display: true,
-            //           color: 'rgba(0, 0, 0, 0.1)',
-            //         },
-            //       },
-            //     ],
-            //   },
-            // },
         };
     },
 
@@ -317,12 +269,12 @@ export default {
             .then((querySnapshot) => {
                 this.visiblestudentCount = querySnapshot.size;
             });
-        db.collection('UserData')
-            .where('role', '==', 'admin')
-            .get()
-            .then((querySnapshot) => {
-                this.hiddenstudentCount -= querySnapshot.size;
-            });
+        // db.collection('UserData')
+        //     .where('role', '==', 'admin')
+        //     .get()
+        //     .then((querySnapshot) => {
+        //         this.visiblestudentCount -= querySnapshot.size;
+        //     });
         // check for superuser
         // db.collection('UserData')
         //   .where('superuser', '==', true)
